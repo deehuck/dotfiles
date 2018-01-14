0bb8e3d9185d5ca412ca3ef85a24733271fe4e41
dhuck <davin@secondbrain.ai>
2018-01-14 03:53:38 -0600

    updated installation script to include installation of neovim python

--- install/deb-install.sh
+++ install/deb-install.sh
@@ -6,6 +6,8 @@ echo "Welcome to deehuck's dotfiles for linux. big ups to mharington for the leg
 
 # Install required programs for sexiness
 echo "Don't worry this repo isn't nefarious. Won't hurt yr apt-get one bit 😘"
+echo "- -- --- ----- --- -- - -- --- ----- --- -- -"
+echo ""
 wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
 echo "deb http://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
 sudo apt-get update
@@ -13,6 +15,9 @@ sudo apt-get update
 
 echo "making sure everything is installed on this machine...
       git, node tmux neovim irssi and rcm htop fortune slurm multitail"
+echo "- -- --- ----- --- -- - -- --- ----- --- -- -"
+echo ""
+
 sudo apt-get -y install git htop fortune nodejs tmux slurm neovim irssi rcm multitail
 
 echo "- -- --- ----- --- -- - -- --- ----- --- -- -"
@@ -20,6 +25,7 @@ echo ""
 echo "Installing necessary pip packages for nvim"
 echo ""
 echo "- -- --- ----- --- -- - -- --- ----- --- -- -"
+
 sudo pip install neovim
 sudo pip3 install neovim
 
@@ -30,25 +36,24 @@ cd $HOME
 git clone https://github.com/deehuck/dotfiles.git ~/.dotfiles
 
 # Set up oh my zsh
-echo "Checking and Install oh-my-zsh.sh"
-
-
-ohmyzsh="$HOME/.oh-my-zsh"
-if [ -f "$ohmyzsh" ]
-then
-  echo "Oh My ZSH! is already installed. Nothing to do"
-else
-  echo "Install Oh My ZSH!"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+#echo "Checking and Install oh-my-zsh.sh"
+#
+#
+#ohmyzsh="$HOME/.oh-my-zsh"
+#if [ -f "$ohmyzsh" ]
+#then
+#  echo "Oh My ZSH! is already installed. Nothing to do"
+#else
+#  echo "Install Oh My ZSH!"
+#  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#fi
 
 
 cd $HOME
-:
+
 echo "running RCM's rcup command"
 echo "This will symlink the rc files in $HOME to those in the dotfiles directory"
 rcup -f
-
 
 echo "--------------------------------------------------------------"
 echo "Changing over to zsh"
@@ -61,8 +66,3 @@ rm -rf .install
 
 
 echo "All done!"
-echo ""
-echo "If some things did not change like expected, you may need to manually symlink"
-echo "some of the files. The rcup command cannot symlink existing files. Delete the"
-echo "existing rc files in $HOME and link them with the command ln -s .dotfiles/[ rc"
-echo " file ] .[rc file]"
