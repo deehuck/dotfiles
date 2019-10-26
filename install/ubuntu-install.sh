ed4912e3d404c785493314bf765e669131485670
dhuck <dhuck@tinycact.us>
2019-10-26 09:57:11 -0500

    beautification continued

--- install/ubuntu-install.sh
+++ install/ubuntu-install.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 checkbox="\e[1m[\e[32m✔\e[39m]\e[34m"
-normal="\e[39m\e0m"
+normal="\e[39m\e[0m\e[25m"
 green="\e[92m"
 blink="\e[5m"
 
 
-echo -e $cyan $blink
-echo "       .___.__                   __   /\            "
-echo "     __| _/|  |__  __ __   ____ |  | _)/ ______     "
-echo "    / __ | |  |  \|  |  \_/ ___\|  |/ / /  ___/     "
-echo "   / /_/ | |   Y  \  |  /\  \___|    <  \___ \      "
-echo "   \____ | |___|  /____/  \___  >__|_ \/____  >     "
-echo "        \/      \/            \/     \/     \/      "
-echo "     .___      __    _____.__.__                    "
-echo "   __| _/_____/  |__/ ____\__|  |   ____   ______   "
-echo "  / __ |/  _ \   __\   __\|  |  | _/ __ \ /  ___/   "
-echo " / /_/ (  <_> )  |  |  |  |  |  |_\  ___/ \___ \    "
-echo " \____ |\____/|__|  |__|  |__|____/\___  >____  >   "
-echo "      \/                               \/     \/    "
-echo " "
+echo -e $green $blink
+echo -e "       .___.__                   __   /\            "
+echo -e "     __| _/|  |__  __ __   ____ |  | _)/ ______     "
+echo -e "    / __ | |  |  \|  |  \_/ ___\|  |/ / /  ___/     "
+echo -e "   / /_/ | |   Y  \  |  /\  \___|    <  \___ \      "
+echo -e "   \____ | |___|  /____/  \___  >__|_ \/____  >     "
+echo -e "        \/      \/            \/     \/     \/      "
+echo -e "     .___      __    _____.__.__                    "
+echo -e "   __| _/_____/  |__/ ____\__|  |   ____   ______   "
+echo -e "  / __ |/  _ \   __\   __\|  |  | _/ __ \ /  ___/   "
+echo -e " / /_/ (  <_> )  |  |  |  |  |  |_\  ___/ \___ \    "
+echo -e " \____ |\____/|__|  |__|  |__|____/\___  >____  >   "
+echo -e "      \/                               \/     \/    "
+echo -e " "
 echo -e $normal
-echo " Welcome to deehuck's dotfiles for linux. big ups to mharington for the legwork"
+echo -e " Welcome to deehuck's dotfiles for linux. big ups to mharington for the legwork"
 
-echo " Installing Repos for neovim and rcm"
-echo " Don't worry this repo isn't nefarious. Won't hurt yr apt-get one bit 😘"
-echo " "
-echo " Please enter your password to add deb http://apt.thoughtbot.com/debian/ "
-echo " to /etc/apt/sources.list.d/ "
+echo -e "$checkbox Installing Repos for neovim and rcm$normal"
+echo -e " Don't worry this repo isn't nefarious. Won't hurt yr apt-get one bit 😘"
+echo -e " "
+echo -e " Please enter your password to add deb http://apt.thoughtbot.com/debian/ "
+echo -e " to /etc/apt/sources.list.d/ "
 
 if [ ! -d ".cache" ]; then
-    echo "Creating cache directory"
+    echo -e "Creating cache directory"
     mkdir $HOME/.cache
 fi
 
@@ -38,23 +38,23 @@ sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm > /dev/null
 sudo apt-add-repository ppa:neovim-ppa/stable > /dev/null
 sudo apt-get update > /dev/null
 
-echo "$checkbox making sure everything is installed on this machine...
+echo -e "$checkbox making sure everything is installed on this machine...
       git, mosh, node tmux neovim irssi and rcm htop fortune slurm multitail"
 
 sudo apt-get -y install git htop fortune nodejs tm/:ux slurm neovim irssi rcm multitail mosh python-pip python3-pip build-essential > /dev/null
 
-echo "$checkbox Installing necessary pip packages for nvim"
+echo -e "$checkbox Installing necessary pip packages for nvim"
 
 cd $HOME
 pip install --user neovim > /dev/null
 pip3 install --user neovim > /dev/null
 
-echo "$checkbox Installing numpy, scipy, pandas, altair, matplotlib "
+echo -e "$checkbox Installing numpy, scipy, pandas, altair, matplotlib "
 pip install numpy, scipy, pandas, altair, matplotlib > /dev/null
 pip3 install numpy, scipy, pandas, altair, matplotlib > /dev/null
 
-echo "$checkbox Cloning the dotfiles to .dotfiles directory"
-echo "$checkbox Feel free to create your own custom branch and make changes"
+echo -e "$checkbox Cloning the dotfiles to .dotfiles directory"
+echo -e "$checkbox Feel free to create your own custom branch and make changes"
 
 git clone https://gitlab.com/dhuck/dotfiles.git ~/.dotfiles > /dev/null
 cd .dotfiles
@@ -62,20 +62,20 @@ git checkout ubuntu > /dev/null
 
 cd $HOME
 
-echo "$checkbox running RCM's rcup command"
-echo "$checkbox This will symlink the rc files in $HOME to those in the dotfiles directory"
+echo -e "$checkbox running RCM's rcup command"
+echo -e "$checkbox This will symlink the rc files in $HOME to those in the dotfiles directory"
 
 rcup -f
 
 #Clean up superfluous files created by rcup
 rm -rf .install
 
-echo "$checkbox Updating nvim plugins"
+echo -e "$checkbox Updating nvim plugins"
 echo -ne '\n' | nvim +qall
 
-echo "$checkbox Setting zsh as default shell"
+echo -e "$checkbox Setting zsh as default shell"
 
 sudo chsh -s /bin/zsh $USER
 
-echo "$checkbox All done! Logout and log back in to apply changes!"
+echo -e "$checkbox All done! Logout and log back in to apply changes!"
 
